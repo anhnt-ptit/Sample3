498dc7a1992f4ff2e8ce88b3fb9d8082d06bb6c1
anhngo <anhngo@192.168.0.139>
2017-07-10 11:51:16 +0700

    test commit

--- Sample3/src/com/sample3/Test31.java
+++ Sample3/src/com/sample3/Test31.java
@@ -6,10 +6,10 @@ public class Test31 {
 		// TODO Auto-generated method stub
 		System.out.println("test31");
 		System.out.println("test31");
+		System.out.println("new Line");
 		System.out.println("test31");
 		System.out.println("test31");
-		System.out.println("new line");
-			
+
 	}
 
 }
