460623460027c6553a75f62c7208dcbcce167eb0
anhnt-ptit <anhnt.ptit@gmail.com>
2017-07-11 11:27:23 +0700

    update from remote host 1
    update from remote host 1

--- Sample3/src/com/sample3/Test3.java
+++ Sample3/src/com/sample3/Test3.java
@@ -5,6 +5,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 555 6666");
+		System.out.println("Test 555 6666");
 
 	}
 
